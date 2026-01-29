9b732bd72eeb2b8447ddafb36016fe7e4f42f33a
Brian Pajares <bpajaresh@hotmail.com>
2026-01-29 12:23:01 +0100

    Calculadora por consola con switch

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,75 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        
+        double numero1 = 0;
+        double numero2 = 0;
+        
+        System.out.println("Ingrese la operacion a realizar (suma, resta, multiplicacion, division):");
+        try (Scanner entrada = new Scanner(System.in)) {
+            String cadena=entrada.nextLine();
+             System.out.println(cadena);
 
-        String nombre = "Brian";
-        int edad = 31;
-        boolean sabeGit = true;
-
-        System.out.println("Hola, me llamo " + nombre + " tengo " +edad + " años.");
-
-        if (sabeGit==true) {
-            System.out.println("Sé usar Git.");
-        } else {
-            System.out.println("No sé usar Git.");
+            switch (cadena) {
+                case "suma" -> {
+                    System.out.println("Ingrese el primer numero:");
+                    numero1=entrada.nextDouble();
+                    System.out.println("Ingrese el segundo numero:");
+                    numero2=entrada.nextDouble();
+                    System.out.println("El resultado de la suma es: " + suma(numero1, numero2));
+                }
+                case "resta" -> {
+                    System.out.println("Ingrese el primer numero:");
+                    numero1=entrada.nextDouble();
+                    System.out.println("Ingrese el segundo numero:");
+                    numero2=entrada.nextDouble();
+                    System.out.println("El resultado de la resta es: " + resta(numero1, numero2));
+                }
+                case "multiplicacion" -> {
+                    System.out.println("Ingrese el primer numero:");
+                    numero1=entrada.nextDouble();
+                    System.out.println("Ingrese el segundo numero:");
+                    numero2=entrada.nextDouble();
+                    System.out.println("El resultado de la multiplicacion es: " + multiplicacion(numero1, numero2));
+                }
+                case "division" -> {
+                    System.out.println("Ingrese el primer numero:");
+                    numero1=entrada.nextDouble();
+                    System.out.println("Ingrese el segundo numero:");
+                    numero2=entrada.nextDouble();
+                    System.out.println("El resultado de la division es: " + division(numero1, numero2));
+                }
+                default -> System.out.println("Operacion no reconocida");
+            }
         }
-
-        mostrarInformacion(nombre, edad); 
-
-
-        boolean puedeEmpezar = puedeEmpezarPracticas(edad, sabeGit);
-
-        if (puedeEmpezar) {
-            System.out.println("Puede empezar las prácticas.");
-        } else {
-            System.out.println("No puede empezar las prácticas.");
-        }
-
     }
 
-
-        static void mostrarInformacion(String nombre, int edad)
-        {
-            System.out.println("Hola, me llamo " + nombre + " tengo " +edad + " años.");
+        public static double suma (double numero1, double numero2) {
+            return numero1 + numero2;
         }
 
-        static boolean puedeEmpezarPracticas(int edad, boolean sabeGit)
-        {
-        return edad >=18 && sabeGit ==true;
+        public static double resta (double numero1, double numero2) {
+            return numero1 - numero2;
         }
+
+        public static double multiplicacion (double numero1, double numero2) {
+            return numero1 * numero2;
+        }
+
+        public static double division (double numero1, double numero2) {
+            if (numero2 != 0) {
+                return numero1 / numero2;
+            } else {
+                System.out.println("No se puede dividir entre 0");
+                return 0;
+            }
+        }
+    }      
+
 
 
 
 
 
     
-}
